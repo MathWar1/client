1b73629d16d782ec8ba5d71d8a2e24011d52d645
ashirogisen <m.rizki.aiman@gmail.com>
2019-11-15 09:51:50 +0700

    interface merged

--- src/router/index.js
+++ src/router/index.js
@@ -21,12 +21,9 @@ const routes = [
     component: () => import(/* webpackChunkName: "inroom" */ '../views/inRoom.vue')
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/gameroom',
+    name: 'gameroom',
+    component: () => import(/* webpackChunkName: "ingame" */ '../views/inGame.vue')
   }
 ]
 
